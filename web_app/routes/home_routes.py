12f8de504d48c3daef511904956f9fa0025e69fe
nimu77 <nirmal.thapa77@outlook.com>
2020-06-23 14:01:39 -0700

    updated routes

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -23,17 +23,17 @@ my_post_list = {4: 'derp_post', 5: 'derpy_post', 6: 'derpen_post'}
 
 @home_routes.route("/predict", methods=["GET", "POST"])
 def predict():
-    samplesub = {
-        "Name": "Connor",
-        "Description": "derpen",
-        "Length of Campaign": 20,
-        "Monetary goal": 10000,
-        "Category": "derp"
-        }
+    # samplesub = {
+        # "Name": "Nick",
+        # "Description": "derpen",
+        # "Length of Campaign": 20,
+        # "Monetary goal": 10000,
+        # "Category": "derp"
+        # }
     if request.method == "POST":
         req = request.get_json()
         print(req)
-        return samplesub, 200
+        return "Thanks", 200
         # return jsonify(my_post_list)
     else:
         return jsonify(my_get_list)
