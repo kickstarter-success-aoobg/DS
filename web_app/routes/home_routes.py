0f0105b7b0df6dc4d2b1195516d0a4ea859f34b8
nimu77 <nirmal.thapa77@outlook.com>
2020-06-22 21:47:14 -0700

    initialized a web app with flask

---
+++ web_app/routes/home_routes.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template, jsonify, request
+home_routes = Blueprint("home_routes", __name__)
+
+@home_routes.route("/")
+def index():
+    return "This is a test environment."
+
+@home_routes.route("/about")
+def about():
+    return """This is where we build a model for prediction of Kickstarter project, 
+              whether they will succeed or fail."""
+
+my_get_list = {1: 'derp', 2: 'derpy', 3: 'derpen'}
+my_post_list = {4: 'derp_post', 5: 'derpy_post', 6: 'derpen_post'}
+
+
+@home_routes.route("/predict", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        return jsonify(my_post_list)
+    else:
+        return jsonify(my_get_list)
