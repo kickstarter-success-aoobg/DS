c6b490889a815b2808d49cfd76daece262ca5fc3
nimu77 <nirmal.thapa77@outlook.com>
2020-06-23 14:27:47 -0700

    updated __init__

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from web_app.routes.home_routes import home_routes
 
@@ -7,7 +8,10 @@ DATABASE_URL = "sqlite:///test_environment.db"
 def create_app():
     app = Flask(__name__)
 
+    cors = CORS(app)
+
     app.register_blueprint(home_routes)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     return app
 
