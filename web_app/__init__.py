0159bb7d80330d90d8397bf4f8542a82ca2a3178
nimu77 <nirmal.thapa77@outlook.com>
2020-06-24 13:15:03 -0700

    imported flask cors to correct error while query

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-
 from web_app.routes.home_routes import home_routes
-
-# DATABASE_URL = "sqlite:///test_environment.db"
 
 def create_app():
     app = Flask(__name__)
