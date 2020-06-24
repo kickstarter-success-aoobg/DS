8b4e7211dab403c2cf7239e4ca56e3d9571c97fd
nimu77 <nirmal.thapa77@outlook.com>
2020-06-24 09:18:07 -0700

    update route

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,14 +1,6 @@
 from flask import Blueprint, render_template, jsonify, request
 home_routes = Blueprint("home_routes", __name__)
-# from flask.ext.cors import CORS
 
-# try:
-#     from flask.ext.cors import CORS  # The typical way to import flask-cors
-# except ImportError:
-#     # Path hack allows examples to be run without installation.
-#     import os
-#     parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#     os.sys.path.insert(0, parentdir)
     
 @home_routes.route("/")
 def index():
@@ -19,8 +11,8 @@ def about():
     return """This is where we build a model for prediction of Kickstarter project, 
               whether they will succeed or fail."""
 
-my_get_list = {1: 'derp', 2: 'derpy', 3: 'derpen'}
-my_post_list = {4: 'derp_post', 5: 'derpy_post', 6: 'derpen_post'}
+# my_get_list = {1: 'derp', 2: 'derpy', 3: 'derpen'}
+# my_post_list = {4: 'derp_post', 5: 'derpy_post', 6: 'derpen_post'}
 
 
 # @home_routes.route("/predict", methods=["GET", "POST"])
@@ -29,9 +21,6 @@ my_post_list = {4: 'derp_post', 5: 'derpy_post', 6: 'derpen_post'}
 #         return jsonify(my_post_list)
 #     else:
 #         return jsonify(my_get_list)
-
-@home_routes.route("/predict", methods=["GET", "POST"])
-def predict():
     # # samplesub = {
     #     "Name": "Nick",
     #     "Description": "derpen",
@@ -39,6 +28,11 @@ def predict():
     #     "Monetary goal": 10000,
     #     "Category": "derp"
     #     }
+
+
+@home_routes.route("/predict", methods=["GET", "POST"])
+def predict():
+
     if request.method == "POST":
         req = request.get_json()
         print(req)
